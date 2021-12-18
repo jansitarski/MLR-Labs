0f666212c6926399aa03b84d9cc799a3e9fb6092
jansitarski00@gmail.com <jansitarski00@gmail.com>
2021-12-18 14:47:47 +0100

    Lab9

---
+++ Lab9/classify.py
@@ -0,0 +1,68 @@
+﻿import os
+
+# Disable TF warning messages
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+import numpy as np
+import cv2  # pip install opencv-python
+
+# Directory with test set
+TEST_DATASET_DIR = 'mnist-test'
+
+# Trained model name
+MODEL = 'model.h5'
+
+# Load trained model of neural network
+model = tf.keras.models.load_model(MODEL)
+
+# Load the images
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+for image_index in range(90, 100):
+
+    # Test image
+    test_image = x_test[image_index]
+    test_image = test_image.astype('float32') / 255.0
+
+    # Classify the input image
+    prediction = model.predict(test_image.reshape(1, 28, 28, 1))
+
+    # Find the winner class and the probability
+    sorted_classes = np.argsort(-1 * prediction)
+    sorted_prediction = -np.sort(-1 * prediction)
+    winner_probability = np.max(sorted_prediction[0][0:] * 100)
+    second_probability = np.max(sorted_prediction[0][1:] * 100)
+    third_probability = np.max(sorted_prediction[0][2:] * 100)
+
+    # Build the text label
+    best_label = "1st = {}, p = {:.2f}%\n".format(sorted_classes[0][0],
+                                                  winner_probability)
+    second_label = "2nd = {}, p = {:.2f}%\n".format(sorted_classes[0][1],
+                                                    second_probability)
+    third_label = "3rd = {}, p = {:.2f}%\n".format(sorted_classes[0][2],
+                                                   third_probability)
+
+    # Draw the label on the image
+    output_image = cv2.resize(test_image, (500, 500))
+    cv2.putText(output_image, best_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
+                0.75, 255, 2)
+    cv2.putText(output_image, second_label, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                0.75, 255, 2)
+    cv2.putText(output_image, third_label, (10, 75), cv2.FONT_HERSHEY_SIMPLEX,
+                0.75, 255, 2)
+    # Show the output image
+
+    cv2.imshow("Output", output_image)
+    cv2.waitKey(0)
+
+# Zadanie 9.2 (1p)
+# Oprócz wyświetlania na obrazach zwycięskiej klasy, wyświetlać dodatkowo
+# klasę z miejsca 2 i 3 (z prawdopodobieństwami)
+
+# Zadanie 9.3 (1.5p)
+# Zamiast wczytywać obrazy testowe z pliku, wykorzystać możliwość załadowania ich
+# metodą mnist.load_data()
+
+# Wynik: plik tekstowy z uzupełnionym kodem 
+#        oraz plik graficzny z przykładowym wynikiem predykcji
---
+++ Lab9/train.py
@@ -0,0 +1,69 @@
+﻿import os
+import matplotlib.pyplot as plt
+# Disable TF warning messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+
+class FullyConnectedForMnist:
+    '''Simple NN for MNIST database. INPUT => FC/RELU => FC/SOFTMAX'''
+    def build(hidden_units):
+        # Initialize the model
+        model = tf.keras.models.Sequential()
+        # Flatten the input data of (x, y, 1) dimension
+        model.add(tf.keras.layers.Flatten(input_shape=(28,28,1)))
+        # FC/RELU layer
+        model.add(tf.keras.layers.Dense(hidden_units, activation='relu'))
+        # Softmax classifier (10 classes)
+        model.add(tf.keras.layers.Dense(10, activation="softmax"))
+        return model
+ 
+EPOCHS = 10
+MODEL_FILENAME = "model.h5"
+
+# Load dataset as train and test sets
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# Convert from uint8 to float32 and normalize to [0,1]
+x_train = x_train.astype('float32')/255
+x_test = x_test.astype('float32')/255
+
+# Transform labels to 0-1 encoding, e.g.
+# 2 -> [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+# 6 -> [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+# Reshape the dataset into 4D array (required by Keras)
+# First dimension is the number of samples
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+# Construct the model
+model = FullyConnectedForMnist.build(500)
+
+# Compile the model and print summary
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+# Train the model
+history = model.fit(x=x_train, y=y_train, epochs=EPOCHS, validation_split=0.2)
+
+# Save model to a file
+model.save(MODEL_FILENAME)
+
+# Evaluate the model on the test data
+model.evaluate(x_test, y_test)
+
+
+# Zadanie 9.1 (1.5p)
+
+# Uzupełnić plik o rysowanie wykresu funkcji kosztu oraz dokładności w funkcji epoki.
+plt.plot(history.history['accuracy'], label="accuracy")
+plt.plot(history.history['loss'], label="cost")
+plt.xlabel("epoch")
+plt.legend()
+#plt.show()
+plt.savefig("plot.png")
+# Wynik: plik tekstowy z uzupełnionym kodem 
+#        oraz plik graficzny z przykładowym wykresem
