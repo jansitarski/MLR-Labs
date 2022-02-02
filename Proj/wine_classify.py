5afa2221ea0b2242de6270df5b809fe6ee2ac4ab
jansitarski00@gmail.com <jansitarski00@gmail.com>
2022-02-02 18:03:59 +0100

    project

--- Proj/wine_classify.py
+++ Proj/wine_classify.py
@@ -1,47 +1,42 @@
 import os
 
-import numpy
-import numpy as np
 import pandas as pd
-from keras.callbacks import ModelCheckpoint
-from keras.initializers.initializers_v1 import TruncatedNormal
-from keras.layers import Dense
-from keras.models import Sequential, load_model
-from matplotlib import pyplot as plt
 from numpy import argmax
-from sklearn import preprocessing, model_selection
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 data = pd.read_csv("winequality-red.csv", sep=";")
-
-data["quality"] = data["quality"].astype(int)
+data = pd.get_dummies(data, columns=["quality"])
 
 X = data.iloc[:, 0:11].values
-X = preprocessing.normalize(X, axis=0)
-Y = np.ravel(data.quality)
+Y = data.iloc[:, 11:].values
 
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,
-                                                                    Y,
-                                                                    test_size=0.33,
-                                                                    random_state=42)
+numberOfQualities = len(Y[0])
+
+scaler = StandardScaler()
+scaler = scaler.fit(X)
+X[:] = scaler.transform(X)
+
+_, X_test, _, Y_test = train_test_split(X, Y, test_size=0.9)
 
 model = keras.models.load_model("BEST_wine_model.h5")
+predictions = model.predict(X_test)
 hit = 0
 miss = 0
-for i in range(1, len(X_test)):
-    data = np.expand_dims(X_test[i], axis=0)
-    label = Y_test[i]
-    # print(data)
-    # model.predict(data)
-    # print(argmax(model.predict(data)))
-    if abs(numpy.round(model.predict(data)[0],decimals=1) - Y_test[i]) < 0.6:
-        #print(numpy.rint(model.predict(data)[0]), " HIT ", Y_test[i])
-        hit+=1
+nearMiss = 0
+for i in range(1, len(predictions)):
+    prediction = predictions[i]
+    label = argmax(Y_test[i])
+    if argmax(prediction) == label:
+        hit += 1
+    elif abs(argmax(prediction) - label) == 1:
+        nearMiss += 1
     else:
-        #print(numpy.round(model.predict(data)[0],decimals=1), " MISS ", Y_test[i])
-        miss+=1
-    # print(Y_test[i][argmax(model.predict(data))])
+        miss += 1
+        print(argmax(predictions[i]), " MISS ", label)
 print("Hit = ", str(hit))
+print("Near Miss = ", str(nearMiss))
 print("Miss = ", str(miss))
--- Proj/wine_train.py
+++ Proj/wine_train.py
@@ -1,102 +1,60 @@
-## https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
-# clf = RandomForestClassifier(n_estimators=10, random_state=20701)
-
 import os
 
-import numpy
 import numpy as np
 import pandas as pd
 from keras.callbacks import ModelCheckpoint
-from keras.initializers.initializers_v1 import TruncatedNormal
 from keras.layers import Dense
 from keras.models import Sequential
 from matplotlib import pyplot as plt
-from numpy import argmax
-from sklearn import preprocessing, model_selection
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-OPTIMIZER = 'adam'
-# LOSS = 'categorical_crossentropy'
-LOSS = 'binary_crossentropy'
-EPOCHS = 800
+np.random.seed(20701)
+EPOCHS = 100
 
-# from kt_utils import *
-
-# Read data from csv default separator is ',', but file uses ';'
-cols = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar",
-        "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density",
-        "pH", "sulphates", "alcohol", "quality"]
 data = pd.read_csv("winequality-red.csv", sep=";")
 
-data["quality"] = data["quality"].astype(int)
-# data = pd.get_dummies(data, columns=["quality"])
-# print(data.head(5))
-
-# X is data columns
-# X = data.iloc[:, 0:11].values
-## Y is quality
-# Y = data.iloc[:, 11:].values
-# Data to [0,1]
-
+# Create columns for each quality [1-10],
+# Note: current dataset has only 6 qualities
+data = pd.get_dummies(data, columns=["quality"])
 
 X = data.iloc[:, 0:11].values
-X = preprocessing.normalize(X, axis=0)
-Y = np.ravel(data.quality)
-# Y = to_categorical(Y, num_classes=10)
+Y = data.iloc[:, 11:].values
 
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,
-                                                                    Y,
-                                                                    test_size=0.33,
-                                                                    random_state=42)
+# Hinders accuracy to 60%, Y = to_categorical(Y, num_classes=10)
 
-# X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, )
+numberOfQualities = len(Y[0])
 
+# Standardize X values
+scaler = StandardScaler()
+scaler = scaler.fit(X)
+X[:] = scaler.transform(X)
 
-# from sklearn.preprocessing import StandardScaler
-# scaler = StandardScaler().fit(X_train)
-# X_train = scaler.transform(X_train)
-# X_test = scaler.transform(X_test)
-
-
-# rint(X_train)
-# rint(X_test)
-# rint(Y_train)
-# rint(Y_test)
-# input()
-
-# inputs = tf.keras.Input(shape=(shape_x,))
-# x = tf.keras.layers.Dense(200, activation='linear')(inputs)
-# outputs = tf.keras.layers.Dense(10, activation='softmax')(x)
-# model = tf.keras.Model(inputs=inputs, outputs=outputs)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
 
 
 model = Sequential()
-init = TruncatedNormal(stddev=0.01, seed=10)
+model.add(Dense(50, activation='relu', input_shape=(11,)))
+model.add(Dense(40, activation='relu'))
+model.add(Dense(30, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(numberOfQualities, activation='softmax'))
 
-# config model
-# model.add(Input(shape=(11,)))
-model = Sequential()
-model.add(Dense(164, input_dim=11, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(1))
-print(model.summary())
-# input()
-
-model.compile(loss='mse',
-              optimizer='rmsprop',
-              metrics=['mse'])
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 checkpoint = ModelCheckpoint(
     filepath='BEST_' + "wine_model.h5",
-    monitor='mse',
+    monitor='val_accuracy',
     verbose=0,
     save_best_only=True,
     save_weights_only=False,
     mode='auto', save_freq='epoch')
 
-# fit
-history = model.fit(x=X_train, y=Y_train, batch_size=8, epochs=EPOCHS,
+history = model.fit(X_train, Y_train, batch_size=8, epochs=EPOCHS, verbose=1,
                     validation_data=(X_test, Y_test), callbacks=[checkpoint])
 
 print("Saving model and plots...")
@@ -104,50 +62,16 @@ model.save("wine_model.h5")
 
 plt.style.use("ggplot")
 plt.figure()
+plt.ylim([0.2, 2])
 plt.plot(range(1, EPOCHS + 1), history.history["loss"], label="train loss")
 plt.plot(range(1, EPOCHS + 1), history.history["val_loss"],
          label="validation loss")
-plt.plot(range(1, EPOCHS + 1), history.history["mse"],
-         label="train mse")
-plt.plot(range(1, EPOCHS + 1), history.history["val_mse"],
+plt.plot(range(1, EPOCHS + 1), history.history["accuracy"],
+         label="train mae")
+plt.plot(range(1, EPOCHS + 1), history.history["val_accuracy"],
          label="validation mse")
 plt.title("Train/validation loss and accuracy")
 plt.xlabel("Epoch #")
-plt.ylabel("Loss")
+plt.ylabel("Metrics")
 plt.legend()
-plt.ylim([0,1])
-plt.savefig("wine.png")
-
-# print("Loss = ", str(preds[0]))
-# print("Test Accuracy = ", str(preds[1]))
-#
-# print(X_train[3])
-# print(Y_train[3])
-# print(model.predict(X_train[3]))
-
-# TODO: Find better classifier
-# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
-# clf = RandomForestClassifier(n_estimators=100, random_state=20701)
-# clf.fit(X_train, Y_train)
-#
-# pred = clf.predict(X_test)
-#
-# for i in range(0, len(Y_test)):
-#    print("Actual : {}".format(argmax(Y_test[i])), end=" - ")
-#    print("Predicted: ", argmax(pred[i]))
-#
-# hit = 0
-# miss = 0
-# for i in range(0, len(Y_test)):
-#   actual_class = Y_test[i]
-#   pred_class = pred[i]
-#   if actual_class != pred_class:
-#       miss += 1
-#       print("Actual: {}".format(actual_class), end=" - ")
-#       print("Predicted: {}".format(pred_class))
-#   else:
-#       hit += 1
-
-# print("\nGood predictions : {}".format(hit))
-# print("Bad predictions : {}".format(miss))
-# print("Total accuracy: \t", clf.score(X_test, Y_test))
+plt.savefig("model.png")
